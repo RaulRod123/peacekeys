22e35224e2d1ee96b9a0b7873d509db6110b8132
RaulRod123 <rodriguezraul502@gmail.com>
2026-03-18 10:00:38 -0500

    Cleaned up lint and Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { loadHistory, saveHistoryRecord } from './utils/historyStorage'
 import ResultsSummary from './components/ResultsSummary'
 import WORD_POOL from './data/wordPool'
 import QUOTE_POOL from './data/quotePool'
+import { Analytics } from '@vercel/analytics/react'
 
 
 
@@ -578,6 +579,7 @@ function App() {
 
           <button onClick={() => restartTest()}>Restart</button>
         </div>
+        <Analytics />
       </div>
     </div>
   )
